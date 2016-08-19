a81bf9575ea0e2dcae4573868c7c52cb2123dd9a
Kot19 <kolya.kovalec@ukr.net>
2016-08-19 23:32:39 +0300

    test comit

---
+++ src/hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Moshe Balanga on 19.08.2016.
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("mkdir hello" +
+                "cd hello" +
+                "touch hello.html");
+    }
+}
